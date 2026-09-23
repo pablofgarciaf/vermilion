cf602c034915f0d350f0bb9d07595e9eeb5fd561
pavelo88 <pablofgarciaf@gmail.com>
2026-09-23 00:36:56 +0000

    fix: conectar NavbarWithReservations en ConditionalNavbar
    El boton MIS RESERVAS no aparecia porque ConditionalNavbar seguia
    importando el Navbar original en vez del que incluye el modal de reservas.

--- components/layout/ConditionalNavbar.tsx
+++ components/layout/ConditionalNavbar.tsx
@@ -1,6 +1,6 @@
 'use client';
 import { usePathname } from 'next/navigation';
-import { Navbar } from './Navbar';
+import { NavbarWithReservations as Navbar } from './NavbarWithReservations';
 
 export function ConditionalNavbar() {
   const pathname = usePathname();
